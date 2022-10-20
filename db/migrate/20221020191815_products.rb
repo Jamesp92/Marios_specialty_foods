6d5b89203c93708c65616b7e9ee98716ea920018
James Perry <PjPerry28@gmail.com>
2022-10-20 12:25:36 -0700

    initial commit , add products table with columns

---
+++ db/migrate/20221020191815_products.rb
@@ -0,0 +1,12 @@
+class Products < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.column(:name, :string)
+      t.column(:country_of_origin, :string)
+      t.column(:cost, :integer)
+      t.column(:review_id, :integer)
+
+      t.timestamps()
+    end
+  end
+end
